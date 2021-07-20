e9b35c3536336f3524f1a819d275740702fd8fe9
Mikolaj Rdzanek <mikolaj.rdzanek@sick.de>
2021-07-20 12:38:51 +0200

    'Update'

--- ImageSeriesFixedLength_InspectorP/scripts/ImageSeriesFixedLength.lua
+++ ImageSeriesFixedLength_InspectorP/scripts/ImageSeriesFixedLength.lua
@@ -1,22 +1,4 @@
---[[----------------------------------------------------------------------------
 
-  Application Name:
-  ImageSeriesFixedLength_InspectorP
-
-  Description:
-  Setting up camera and taking image series of fixed length started by external trigger.
-
-  How to Run:
-  A connected InpspectorP6 device with a connected trigger source is necessary to run
-  this sample. Starting this sample is possible either by running the app (F5) or
-  debugging (F7+F10).
-  Set a breakpoint on the first row inside the main or processImage function to
-  debug step-by-step. See the acquired images in the image viewer on the DevicePage.
-
-  More Information:
-  See the tutorial "Devices - InspectorP - TriggeringAndAcquisition".
-
-------------------------------------------------------------------------------]]
 --Start of Global Scope---------------------------------------------------------
 
 -- Create and configure camera
